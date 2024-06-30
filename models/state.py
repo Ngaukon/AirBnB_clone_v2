098662911734a5ceb0c54d7a052f4e6878ddf326
Ngaukon <josephlucia100@gmail.com>
2024-06-30 03:07:48 -0700

    static 1

---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This modules creates the User class"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Class for managing state objects"""
+
+    name = ""
