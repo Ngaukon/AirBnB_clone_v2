650dd6c527c3df8c6e5b59ac28a4dca03722d455
Ngaukon <josephlucia100@gmail.com>
2024-07-28 10:58:28 -0700

    hello route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-starts the Flask web applications
+starts a Flask web application
 """
 
 from flask import Flask
@@ -11,6 +11,12 @@ app = Flask(__name__)
 def index():
     """returns Hello HBNB!"""
     return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns HBNB"""
+    return 'HBNB'
 
 
 if __name__ == '__main__':
