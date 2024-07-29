8a84e0e02500674fa3a97240b064782295811c49
Ngaukon <josephlucia100@gmail.com>
2024-07-29 13:42:46 -0700

    update base moedle

--- models/base_model.py
+++ models/base_model.py
@@ -6,18 +6,14 @@ Contains class BaseModel
 from datetime import datetime
 import models
 from os import getenv
-import sqlalchemy
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 import uuid
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
 
-if models.storage_t == "db":
-    Base = declarative_base()
-else:
-    Base = object
-
+# Use a base class for SQLAlchemy models
+Base = declarative_base() if models.storage_t == "db" else object
 
 class BaseModel:
     """The BaseModel class from which future classes will be derived"""
@@ -32,15 +28,15 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != "__class__":
                     setattr(self, key, value)
-            if kwargs.get("created_at", None) and type(self.created_at) is str:
+            if kwargs.get("created_at"):
                 self.created_at = datetime.strptime(kwargs["created_at"], time)
             else:
                 self.created_at = datetime.utcnow()
-            if kwargs.get("updated_at", None) and type(self.updated_at) is str:
+            if kwargs.get("updated_at"):
                 self.updated_at = datetime.strptime(kwargs["updated_at"], time)
             else:
                 self.updated_at = datetime.utcnow()
-            if kwargs.get("id", None) is None:
+            if kwargs.get("id") is None:
                 self.id = str(uuid.uuid4())
         else:
             self.id = str(uuid.uuid4())
