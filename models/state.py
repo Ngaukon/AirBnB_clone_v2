9c7e48f724516045212ecde15f0b81dc40ec41b3
Ngaukon <josephlucia100@gmail.com>
2024-07-29 12:35:13 -0700

    updating the state

--- models/state.py
+++ models/state.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Defines the State class"""
-
+"""Holds the State class"""
 import models
 from models.base_model import BaseModel, Base
 from models.city import City
@@ -11,39 +10,38 @@ from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """Representation of a State in the database or in memory"""
+    """Representation of a State in the database"""
 
-    # Use SQLAlchemy ORM mapping if storage_t is set to "db"
     if models.storage_t == "db":
         __tablename__ = 'states'
-        
-        # Name of the state
+        # State name
         name = Column(String(128), nullable=False)
-        
-        # Relationship to the City class (one-to-many)
+        # Relationship with City: one-to-many relationship
         cities = relationship("City", backref="state")
     else:
-        # In-memory representation when not using a database
+        # In filesystem storage mode, name is a simple attribute
         name = ""
 
     def __init__(self, *args, **kwargs):
-        """Initializes a State instance"""
+        """Initializes a new State instance"""
         super().__init__(*args, **kwargs)
 
     if models.storage_t != "db":
         @property
         def cities(self):
-            """Returns a list of City instances related to the state
+            """
+            Getter for a list of City instances related to the State.
 
-            The list is built by filtering all cities stored in the in-memory storage 
-            to find those associated with the current state instance.
+            This method is used only in filesystem storage mode where
+            relationships are not handled by SQLAlchemy.
 
             Returns:
-                list: List of City instances related to the current State
+                list: A list of City instances related to the current State.
             """
             city_list = []
-            all_cities = models.storage.all(City)  # Get all cities from storage
+            all_cities = models.storage.all(City)
             for city in all_cities.values():
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
+
