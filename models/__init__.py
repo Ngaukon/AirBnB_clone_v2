190de39987ce4f23472478f95b38baf6a2103b79
Ngaukon <josephlucia100@gmail.com>
2024-07-29 12:57:45 -0700

    init update

--- models/__init__.py
+++ models/__init__.py
@@ -5,17 +5,13 @@ initialize the models package
 
 from os import getenv
 
-def setup_storage():
-    """Initialize storage based on the environment variable"""
-    global storage
-    storage_t = getenv("HBNB_TYPE_STORAGE")
-    if storage_t == "db":
-        from models.engine.db_storage import DBStorage
-        storage = DBStorage()
-    else:
-        from models.engine.file_storage import FileStorage
-        storage = FileStorage()
-    storage.reload()
 
-# Call the setup function to initialize storage
-setup_storage()
+storage_t = getenv("HBNB_TYPE_STORAGE")
+
+if storage_t == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+storage.reload()
